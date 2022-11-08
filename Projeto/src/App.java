3592c1b988776e8831eda2891ff676e4e1c5543a
Silvio <silvioeduardosbr@gmail.com>
2022-11-08 05:37:34 -0300

    ddd

--- Projeto/src/App.java
+++ Projeto/src/App.java
@@ -1,5 +1,5 @@
-public class App {
+public class App { 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello,d World!");
+        System.out.println("Helssslo, World!");
     }
 }
