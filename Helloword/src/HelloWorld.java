aaab2b4c961c234ea6a95385e2bc70e6b75948a5
Silvio <silvioeduardosbr@gmail.com>
2022-11-16 17:45:56 -0300

    Meu pc

--- Helloword/src/HelloWorld.java
+++ Helloword/src/HelloWorld.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class HelloWorld { 
     public static void main(String[] args) {
-        System.out.println("Hellow, world!");
+        
+        Scanner input = new Scanner(System.in);
+
+        String nome = input.nextLine();
+        float nota = input.nextFloat();
+
+        System.out.format("A nota do %s é %.2f", nome, nota); // print linha
+       // System.out.printf("A sua nota é %.2f", nota); // print formatado
+        // System.out.format("A sua nota é %.2f", nota); // mesma coisa
+
+        
 
     }
 }
