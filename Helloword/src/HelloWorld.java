c77a4fff18bf9b39448e3290b5ed065cba079b1f
Silvio Eduardo <silvioeduardosbr@gmail.com>
2022-11-21 13:34:18 -0300

    Estudos java arrays, loops, operador ternario etc

--- Helloword/src/HelloWorld.java
+++ Helloword/src/HelloWorld.java
@@ -1,11 +1,34 @@
+import javax.print.attribute.standard.MediaSize.JIS;
+import javax.swing.InputMap;
+
 public class HelloWorld { 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hellow, world!");
-=======
-        System.out.println("Hellow, !");
+        
+        int a = 100;
 
->>>>>>> db4e318ee83e34ff0a2328efd2b06fc99b6a0dff
+        int[] arr = new int[5]; // array simples
 
+        int[][] arr2 = new int [3][3]; // matriz 3x3
+        
+
+
+        arr[2] = 10;
+
+        System.out.println(arr[2]);
+        
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("A");
+        }
+
+        for(int i: arr){ // For sem acesso aos indices
+            System.out.println("ASAS");
+        }
+
+        for (int i = 0; i < arr2.length; i++) {
+            for (int j = 0; j < arr2[i].length; j++) {
+                System.out.print("aaA ");
+            }
+            System.out.println("\n");
+        }
     }
 }
