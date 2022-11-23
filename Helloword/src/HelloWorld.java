c783a848ac76599761c559340de82c6f0cc2bd1e
Silvio Eduardo <silvioeduardosbr@gmail.com>
2022-11-23 08:19:24 -0300

    Commits pc larces

--- Helloword/src/HelloWorld.java
+++ Helloword/src/HelloWorld.java
@@ -1,5 +1,3 @@
-import javax.print.attribute.standard.MediaSize.JIS;
-import javax.swing.InputMap;
 
 public class HelloWorld { 
     public static void main(String[] args) {
@@ -10,9 +8,18 @@ public class HelloWorld {
 
         int[][] arr2 = new int [3][3]; // matriz 3x3
         
+        int[][][] arr3 = new int [3][3][3];
+
+        arr2[2][2] = 9;
 
 
-        arr[2] = 10;
+        arr[0] = 10;
+        arr[1] = 6;
+        arr[2] = 4;
+        arr[3] = 6;
+        arr[4] = 7;
+        
+
 
         System.out.println(arr[2]);
         
@@ -29,6 +36,24 @@ public class HelloWorld {
                 System.out.print("aaA ");
             }
             System.out.println("\n");
+        }
+
+
+        for (int[] i: arr2 ){
+            for (int j: i){
+                System.out.print(j);
+            }
+            System.out.println("\n");
+        }
+
+        int[][] arrayInt = new int[3][];
+
+        arrayInt[0] = new int[2]; //posição 0 recebe um array de 2 posições
+        arrayInt[1] = new int[3];
+        arrayInt[2] = new int[5];
+        
+        for(int[] i : arrayInt){
+            System.out.println(i);
         }
     }
 }
