b3ff5fbfae4396222e6d32382fcf1f7f563c2300
Silvio Eduardo Sales <silvioeduardosbr@gmail.com>
2022-11-24 12:16:29 -0300

    visibilidade de atributos (private, public)

---
+++ Java_POO/src/ProjetoTeste/App.java
@@ -0,0 +1,11 @@
+package ProjetoTeste;
+
+public class App {
+    public static void main(String[] args) {    
+    
+        Conta c1 = new Conta();
+
+        c1.entrar();
+        c1.assistirFilme("VENO");
+    }
+}
---
+++ Java_POO/src/ProjetoTeste/Conta.java
@@ -0,0 +1,36 @@
+package ProjetoTeste;
+
+public class Conta{
+    private String idiomaPreferencial;
+    private String resolucaoTela;
+
+    
+    void entrar(){
+        buscarPreferenciasUsuario();
+        ididentificarResolucao();
+    }
+
+    private void buscarPreferenciasUsuario(){
+        idiomaPreferencial = "PT-BR";
+
+    }
+
+    private void ididentificarResolucao(){
+        resolucaoTela = "Full HD";
+    }
+
+    void assistirFilme(String nomeFilme){
+        System.out.println("Iniciando o filme "+nomeFilme);
+        System.out.println("Carregando o filme na resolucao "+ resolucaoTela);
+        carregandoAudioFilme();
+    }
+
+    private void carregandoAudioFilme(){
+        if(idiomaPreferencial == "PT-BR" || idiomaPreferencial == "EN-US"){
+            System.out.println("Carregando em "+ idiomaPreferencial);
+        }else 
+        {
+            System.out.println("Carregando em EN-US");
+        }
+    }
+}
